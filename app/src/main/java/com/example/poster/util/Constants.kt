3af0ef8a670dca247117eeb305d2135e0d35c012
Does Later <doeslater@gmail.com>
2025-04-19 00:56:03 +0300

    Update News UI to limit the number of displayed headline news items

--- app/src/main/java/com/example/poster/util/Constants.kt
+++ app/src/main/java/com/example/poster/util/Constants.kt
@@ -7,5 +7,6 @@ class Constants {
         const val BASE_URL_WEATHER = "https://api.openweathermap.org/data/2.5/"
         const val API_KEY_WEATHER_API = "add your own api key"
         const val WEATHER_LOCATION = "London"
+        const val DISPLAY_HEADLINE_NEWS_ITEMS  = 6
     }
 }
